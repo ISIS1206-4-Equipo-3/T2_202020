2e5f2930e6898f0ec4f129c9cbdcf5b36d122522
Alejandro Ahogado <a.ahogado@uniandes.edu.co>
2020-08-31 02:11:17 -0500

    Inicio método sort

--- src/modeloEstructuraDatos/ShellSort.java
+++ src/modeloEstructuraDatos/ShellSort.java
@@ -2,4 +2,24 @@ package modeloEstructuraDatos;
 
 public class ShellSort {
 
+	public static void sort (Comparable[] array)
+	{
+		int N = array.length;
+		int h = 1;
+		while (h < N/3)
+			h = 3*h +1;
+		while ( h>=1)
+		{
+			for(int i=h; i<N; i++)
+			{
+				for (int j = i; j <=h ; j -=h) {
+					
+				}
+			}
+			h = h/3;
+		}
+	}
+	
+	
+	
 }
